35420f031f051868f15203c8a6ef89fecbd51277
Prakash Raut <prakash_raut@outlook.com>
2025-08-04 12:14:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-//your JS code here. If required.
 const input = document.getElementById('ip');
 const output = document.getElementById('output');
 const button = document.getElementById('btn');
 
 button.addEventListener('click', () => {
   output.textContent = '';
+  const num = Number(input.value);
   new Promise((resolve) => {
     setTimeout(() => {
-      output.textContent = input.value;
-      resolve(input.value);
+      output.textContent = `Result: ${num}`;
+      resolve(num);
     }, 2000);
   })
     .then(value => {
       return new Promise((resolve) => {
         setTimeout(() => {
-          output.textContent = value * 2;
-          resolve(value * 2);
+          const result = value * 2;
+          output.textContent = `Result: ${result}`;
+          resolve(result);
         }, 2000);
       })
     })
     .then(value => {
       return new Promise((resolve) => {
         setTimeout(() => {
-          output.textContent = value - 3;
-          resolve(value - 3);
+          const result = value - 3;
+          output.textContent = `Result: ${result}`;
+          resolve(result);
         }, 1000);
       })
     })
     .then(value => {
       return new Promise((resolve) => {
         setTimeout(() => {
-          output.textContent = value / 2;
-          resolve(value / 2);
+          const result = value / 2;
+          output.textContent = `Result: ${result}`;
+          resolve(result);
         }, 1000);
       })
     })
     .then(value => {
       return new Promise((resolve) => {
         setTimeout(() => {
-          output.textContent = value + 10;
-          resolve(value + 10);
+          const result = value + 10;
+          output.textContent = `Final Result: ${result}`;
+          resolve(result);
         }, 1000);
       })
     })
