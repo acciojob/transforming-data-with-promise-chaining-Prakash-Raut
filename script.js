d439e7885a350aa448a6b6f3195a16692d3c732f
Prakash Raut <prakash_raut@outlook.com>
2025-08-04 12:05:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your JS code here. If required.
+const input = document.getElementById('ip');
+const output = document.getElementById('output');
+const button = document.getElementById('btn');
+
+button.addEventListener('click', () => {
+  output.textContent = '';
+  new Promise((resolve) => {
+    setTimeout(() => {
+      output.textContent = input.value;
+      resolve(input.value);
+    }, 2000);
+  })
+    .then(value => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          output.textContent = value * 2;
+          resolve(value * 2);
+        }, 2000);
+      })
+    })
+    .then(value => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          output.textContent = value - 3;
+          resolve(value - 3);
+        }, 1000);
+      })
+    })
+    .then(value => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          output.textContent = value / 2;
+          resolve(value / 2);
+        }, 1000);
+      })
+    })
+    .then(value => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          output.textContent = value + 10;
+          resolve(value + 10);
+        }, 1000);
+      })
+    })
+})
